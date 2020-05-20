89e0d4279569ddae393a9336672569da2badc4d7
Chính Trung <32433471+hchinhtrung@users.noreply.github.com>
2020-05-20 08:25:30 +0700

    format

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
 /*!
-    * Start Bootstrap - Agency v6.0.0 (https://startbootstrap.com/template-overviews/agency)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
-    */
-    (function ($) {
+ * Start Bootstrap - Agency v6.0.0 (https://startbootstrap.com/template-overviews/agency)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
+ */
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
+            this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
+            target = target.length ?
+                target :
+                $("[name=" + this.hash.slice(1) + "]");
             if (target.length) {
-                $("html, body").animate(
-                    {
+                $("html, body").animate({
                         scrollTop: target.offset().top - 72,
                     },
                     1000,
