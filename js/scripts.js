3099d037cd4fcd5257699ebb14600142496ae5dd
Chính Trung <32433471+hchinhtrung@users.noreply.github.com>
2020-06-01 16:52:48 +0700

    latestMonday

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-/*!
- * Start Bootstrap - Agency v6.0.0 (https://startbootstrap.com/template-overviews/agency)
- * Copyright 2013-2020 Start Bootstrap
- * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
- */
 (function ($) {
     "use strict"; // Start of use strict
 
